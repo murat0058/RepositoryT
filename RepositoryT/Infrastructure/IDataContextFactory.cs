0e4adba2a9c7f02700b266dd54fb821b7f6be10e
Ömer Cinbat <ocinbat@outlook.com>
2016-05-26 18:46:02 +0300

    remove context manager

--- RepositoryT/Infrastructure/IDataContextFactory.cs
+++ RepositoryT/Infrastructure/IDataContextFactory.cs
@@ -5,7 +5,5 @@ namespace RepositoryT.Infrastructure
     public interface IDataContextFactory<out TContext> : IDisposable where TContext : IDisposable
     {
         TContext GetContext();
-        void Create();
-        void Release();
     }
 }
