4e655a74a77f26579626b38f2dff4b04af2eaa1c
Ömer Cinbat <ocinbat@outlook.com>
2016-05-26 18:32:00 +0300

    IContextManager update.

--- RepositoryT/Infrastructure/IContextManager.cs
+++ RepositoryT/Infrastructure/IContextManager.cs
@@ -1,6 +1,8 @@
-﻿namespace RepositoryT.Infrastructure
+﻿using System;
+
+namespace RepositoryT.Infrastructure
 {
-    public interface IContextManager
+    public interface IContextManager : IDisposable
     {
         void Create();
         void Release();
