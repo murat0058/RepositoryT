de98f478856eda57d64be39a7835645e4c5299a0
Ömer Cinbat <ocinbat@outlook.com>
2016-06-12 19:23:29 +0300

    Autopublish test.

--- RepositoryT/Extensions/IOrderable.cs
+++ RepositoryT/Extensions/IOrderable.cs
@@ -7,11 +7,17 @@ namespace RepositoryT.Extensions
     public interface IOrderable<T> where T : class
     {
         IQueryable<T> AsQueryable();
+
         IOrderable<T> Asc<TK>(Expression<Func<T, TK>> selector);
+
         IOrderable<T> Asc<TK1, TK2>(Expression<Func<T, TK1>> selector1, Expression<Func<T, TK2>> selector2);
+
         IOrderable<T> Asc<TK1, TK2, TK3>(Expression<Func<T, TK1>> selector1, Expression<Func<T, TK2>> selector2, Expression<Func<T, TK3>> selector3);
+
         IOrderable<T> Desc<TK>(Expression<Func<T, TK>> keySelector);
+
         IOrderable<T> Desc<TK1, TK2>(Expression<Func<T, TK1>> selector1, Expression<Func<T, TK2>> selector2);
+
         IOrderable<T> Desc<TK1, TK2, TK3>(Expression<Func<T, TK1>> selector1, Expression<Func<T, TK2>> selector2, Expression<Func<T, TK3>> selector3);
     }
 }
